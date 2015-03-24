97162b5333db9b4738f07243d6c3a76f5c915110
Thomas G <tomgelder@gmail.com>
2015-03-24 14:13:54 -0700

    fixed sharias

--- StateArrays.js
+++ StateArrays.js
@@ -1,11 +1,15 @@
-// Don't forget to adjust these for the starting line
+// Don't forget to adjust these for the starting row
 
-// Abuja is Last
+// FCT-Abuja is Last
 var alphabetical = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37];
-// There are 11 Sharia States
-var shariaStates = [5,8,15,18,19,20,21,26,33,35,36];
-// There are 26 Non-Sharia States including FCT
-var nonSharia    = [1,2,3,4,6,7,9,10,11,12,13,14,16,17,22,23,24,25,27,28,29,30,31,32,34,37];
+var states = ["Abia","Adamawa","Akwa Ibom","Anambra","Bauchi","Bayelsa","Benue","Borno","Cross River","Delta","Ebonyi","Edo","Ekiti","Enugu","Gombe","Imo","Jigawa","Kaduna","Kano","Katsina","Kebbi","Kogi","Kwara","Lagos","Nasarawa","Niger","Ogun","Ondo","Osun","Oyo","Plateau","Rivers","Sokoto","Taraba","Yobe","Zamfara", "FCT-Abuja"];
+
+// There are 12 Sharia States
+// Bauchi, Borno, Gombe, Jigawa, Kaduna, Kano, Katsina, Kebbi, Niger, Sokoto, Yobe, Zamfara
+var shariaStates = [5,8,15,17,18,19,20,21,26,33,35,36];
+// There are 24 Non-Sharia States and FCT
+// Abia, Adamawa, Akwa Ibom, Anambra, Bayesla, Benue, Cross River, Delta, Ebonyi, Edo, Ekiti, Enugu, Imo, Kogi, Kwara, Lagos, Nasarawa, Ogun, Ondo, Osun, Oyo, Plateau, Rivers, Taraba, FCT-Abuja
+var nonSharia    = [1,2,3,4,6,7,9,10,11,12,13,14,16,22,23,24,25,27,28,29,30,31,32,34,37];
 
 // Zones
 NE = [2,5,8,15,34,35];
